bb66f0d37405cc9059eda1b0d4230e1639592cec
yuliabgkv <bugakovayk@gmail.com>
2023-09-21 00:02:20 +0300

    Updated styles, code refactoring

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const ACTIVE_CLASS = 'active';
 const addForm = document.querySelector('#addForm');
 const todoInput = document.querySelector('#todoInput');
 const list = document.querySelector('#list');
-const listItemTemplate = document.querySelector('#list-item-template');
-const actionsBar = document.querySelector('#actionsBar');
+const listItemTemplate = document.querySelector('#listItemTemplate');
+const clearCompletedBtn = document.querySelector('#clearCompletedBtn');
+const filterBtns = document.querySelector('#filterBtns');
 const leftCounter = document.querySelector('#leftCounter');
 const filters = {
     all: 'all',
@@ -19,55 +20,60 @@ let todos = [];
 init();
 
 list.addEventListener('click', onListClick);
-actionsBar.addEventListener('click', onActionsBarClick);
+clearCompletedBtn.addEventListener('click', onClearCompletedBtnClick);
+filterBtns.addEventListener('click', onFilterBtnsClick);
 addForm.addEventListener('submit', submitAddForm);
-
 
 function onListClick(e) {
     const currItemElement = e.target.closest('.list-item');
-    const key = currItemElement.dataset.itemId;
-
-    if(e.target.classList.contains('delete-btn')) {
-        api.deleteItem(key);
-        todos = todos.filter(todo => todo.id != key);
-        currItemElement.remove();
-        updateCounter();
-    } else if(currItemElement) {
-        const currItem = todos.find(todo => todo.id == key);
-        currItem.progress = currItem.progress === 'active' ? 'completed' : 'active';
-        currItemElement.classList.toggle(LIST_ITEM_COMPLETED_CLASS);
-
-        if(currFilter !== currItem.progress && currFilter !== 'all')
+    if(currItemElement) {
+        const key = currItemElement.dataset.itemId;
+    
+        if(e.target.classList.contains('delete-btn')) {
+            api.deleteItem(key);
+            todos = todos.filter(todo => todo.id != key);
             currItemElement.remove();
-
-        api.addItem(currItem);
-        updateCounter();
+            updateCounter();
+        } else {
+            const currItem = todos.find(todo => todo.id == key);
+            currItem.progress = currItem.progress === 'active' ? 'completed' : 'active';
+            currItemElement.classList.toggle(LIST_ITEM_COMPLETED_CLASS);
+    
+            if(currFilter !== currItem.progress && currFilter !== 'all')
+                currItemElement.remove();
+    
+            api.addItem(currItem);
+            updateCounter();
+        }
     }
 }
 
-function onActionsBarClick(e) {
+function onClearCompletedBtnClick() {
+    const completedItems = todos.filter(todo => todo.progress === 'completed');
+    completedItems.map(item => {
+        api.deleteItem(item.id)
+    });
+
+    todos = todos.filter(todo => todo.progress === 'active');
+    list.innerHTML = '';
+    let items = (currFilter == filters.completed) ? filterList(filters.completed) : todos;
+    items.map(renderTodo);
+}
+
+function onFilterBtnsClick(e) {
     const target = e.target;
     const btnIsActive = target.classList.contains(ACTIVE_CLASS);
 
-    if(target.classList.contains('clear-completed-btn')) {
-        const completedItems = todos.filter(todo => todo.progress === 'completed');
-        completedItems.map(item => {
-            api.deleteItem(item.id)
-        });
-
-        todos = todos.filter(todo => todo.progress === 'active');
-        list.innerHTML = '';
-        todos.map(renderTodo);
-    } else if(target.classList.contains('filter-btn') && !btnIsActive) {
+    if(target.classList.contains('filter-btn') && !btnIsActive) {
         if(target.classList.contains('all-btn')) {
             currFilter = filters.all;
-            filterList(target);
+            renderFilter(target);
         } else if(target.classList.contains('active-todos-btn')) {
             currFilter = filters.active;
-            filterList(target);
+            renderFilter(target);
         } else if(target.classList.contains('completed-todos-btn')) {
             currFilter = filters.completed;
-            filterList(target);
+            renderFilter(target);
         }
     }
 }
@@ -113,7 +119,7 @@ function parseObjToArr(obj) {
     });
 }
 
-function filterList(currBtn) {
+function filterList() {
     list.innerHTML = '';
     let itemsList;
 
@@ -124,15 +130,19 @@ function filterList(currBtn) {
     else if(currFilter === filters.completed)
         itemsList = todos.filter(todo => todo.progress === 'completed');
 
-    itemsList.map(renderTodo);
+    return itemsList;
+}
 
-    actionsBar.querySelector('.active').classList.remove(ACTIVE_CLASS);
+function renderFilter(currBtn) {
+    const itemsList = filterList();
+    itemsList.map(renderTodo);
+    filterBtns.querySelector('.active').classList.remove(ACTIVE_CLASS);
     currBtn.classList.add(ACTIVE_CLASS);
 }
 
 function renderTodo(todo) {
     const clone = listItemTemplate.content.cloneNode(true);
-    clone.querySelector('#item-content').textContent = todo.value;
+    clone.querySelector('#itemContent').textContent = todo.value;
     clone.querySelector('li').dataset.itemId = todo.id;
 
     if(todo.progress === 'completed')
