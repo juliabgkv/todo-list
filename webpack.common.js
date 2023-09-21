27607d99ce8f021e8b23950d5a60dec5bd489798
yuliabgkv <bugakovayk@gmail.com>
2023-09-21 13:46:17 +0300

    Updated settings

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,6 @@ module.exports = {
         path: path.resolve(__dirname, './build'),
         filename: '[name].bundle.js'
     },
-    mode: 'development',
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html')
